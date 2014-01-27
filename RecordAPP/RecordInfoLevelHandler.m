f90437d7ed749ff3642b04e7a8670498995b3dd4
Jay Pan <sfffaaa@gmail.com>
2014-01-27 23:39:53 +0800

    Adding view, structure and flow

--- RecordAPP/RecordInfoLevelHandler.m
+++ RecordAPP/RecordInfoLevelHandler.m
@@ -7,7 +7,50 @@
 //
 
 #import "RecordInfoLevelHandler.h"
+#import "DebugUtil.h"
+#import "RecordInfo.h"
+
+@interface RecordInfoLevelHandler()
+
+@property (nonatomic, strong) RecordInfo* recordInfo;
+@end
 
 @implementation RecordInfoLevelHandler
+@synthesize recordInfo = _recordInfo;
+
+- (id) init
+{
+    self = [super init];
+    if (nil != self) {
+        _recordInfo = [[RecordInfo alloc] init];
+    }
+    return self;
+}
+
+- (BOOL) setRecordInfoName
+{
+    CHECK_NOT_ENTER_HERE;
+    return FALSE;
+}
+
+- (BOOL) submit
+{
+    CHECK_NOT_ENTER_HERE
+    return FALSE;
+}
+
+- (BOOL) listen
+{
+    //goto listen page
+    CHECK_NOT_ENTER_HERE
+    return FALSE;
+}
+
+- (BOOL) recordAgain
+{
+    //goto record page
+    CHECK_NOT_ENTER_HERE
+    return FALSE;
+}
 
 @end
